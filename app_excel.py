093b16cc94531699419d16c5dff572806e749ebc
shaniangel <shaniangel97@gmail.com>
2024-10-12 16:09:20 +0300

    add gemini to excel page, add instructions for uploading file + minor changes

--- app_excel.py
+++ app_excel.py
@@ -16,21 +16,38 @@ def main():
         selected_index_config = next(config for config in index_configurations if config['index_name'] == selected_index_name)
         selected_embedding_model_name = selected_index_config['embedding_model']
         embedding_model = sentence_transformer_models[selected_embedding_model_name]
-        rag_models = {"Cohere (command-r-plus)": "command-r-plus", "GPT-2": "gpt2",
-                      "Qwen2.5-0.5B-Instruct": "Qwen2.5-0.5B-Instruct"}
+        rag_models = {"Gemini-1.5-flash": "Gemini-1.5-flash",
+                      "Qwen2.5-0.5B-Instruct": "Qwen2.5-0.5B-Instruct",
+                      "Cohere (command-r-plus)": "command-r-plus"}
         selected_rag_model = st.selectbox("Select RAG Model", rag_models.keys())
         # Checkbox for reranking
         use_reranking = st.checkbox("Use reranking", value=False)
         # Checkbox for rewriting the query
-        use_rewrite = st.checkbox("Rewrite the query", value=False)
+        use_rewrite = st.checkbox("Rephrase the query", value=False)
 
-    # File upload and processing
-    uploaded_file = st.file_uploader("Upload Excel file", type="xlsx")
+
+    # Main area for user query
+    col1, col2 = st.columns([1, 10])
+    with col1:
+        st.image('logo.jpeg')
+    with col2:
+        st.title("Terms and Services Query Interface")
+
+    # Info icon/message
+    st.info("**Instructions for Uploading Excel File:**\n"
+            "1. The uploaded file must be in Excel format (.xlsx).\n"
+            "2. Ensure the following columns are present:\n"
+            "   - **company**: Name of the company.\n"
+            "   - **question**: The question related to the Terms & Conditions.\n"
+            "   - **right answer**: The correct answer to the question.\n"
+            "3. Please ensure that there are no empty rows in the file.")
+
+    uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
     if uploaded_file:
         df = pd.read_excel(uploaded_file)
         st.write("Preview of the uploaded file:", df.head())
 
-        if not check_excel_valid(df,load_available_companies):
+        if not check_excel_valid(df, load_available_companies):
             st.stop()
 
         else:
@@ -41,7 +58,7 @@ def main():
                 right_answer = row['right answer']
                 original_query = query
                 if use_rewrite:
-                    query = rewrite_query(query, cohere_api_key)
+                    query = rewrite_query(query, hf_models)
                 query_embedding = embedding_model.encode([query], convert_to_tensor=True).tolist()[0]
                 index = pc.Index(selected_index_name)
                 top_k = 5 if use_reranking else 3
@@ -50,23 +67,25 @@ def main():
                     reranked_results = rerank_documents(query, results["matches"], top_n=3)
                     context = "\n".join([result["metadata"]["text"] for result in reranked_results])
                     numbered_context = "\n".join(
-                        [f"{i + 1}. {item['metadata']['text']}" for i, item in enumerate(reranked_results)])
+                        [f"{i + 1}. {item['metadata']['text']}\n" for i, item in enumerate(reranked_results)])
                 else:
                     context = "\n".join([result["metadata"]["text"] for result in results["matches"]])
                     numbered_context = "\n".join(
-                        [f"{i + 1}. {item['metadata']['text']}" for i, item in enumerate(results["matches"])])
+                        [f"{i + 1}. {item['metadata']['text']}\n" for i, item in enumerate(results["matches"])])
 
-                rag_answer = generate_answer(selected_rag_model, query, context, cohere_api_key, hf_models)
-                direct_answer = generate_answer(selected_rag_model, query, "", cohere_api_key, hf_models)
+                rag_answer = generate_answer(selected_rag_model, query, context, cohere_api_key, hf_models, True)
+                direct_answer = generate_answer(selected_rag_model, query, "", cohere_api_key, hf_models, False)
                 response = {'Question': original_query, 'Company': company, 'Context': numbered_context, 'RAG Answer': rag_answer, "Direct Answer": direct_answer, "Right Answer": right_answer}
                 if use_rewrite:
-                    response['Rewritten Query'] = query
+                    response['Rephrased Question'] = query
                 responses.append(response)
 
-
             result_df = pd.DataFrame(responses)
+            if use_rewrite:
+                result_df = result_df[['Question', 'Rephrased Question', 'Company', 'Context', 'RAG Answer', 'Direct Answer', 'Right Answer']]
+            st.write("Preview of the results:", result_df.head())
+
             output_file = 'responses.xlsx'
             result_df.to_excel(output_file, index=False)
 
-            st.write(f"Download the responses file:")
-            st.download_button(label="Download Excel file", data=open(output_file, 'rb').read(), file_name=output_file, mime="application/vnd.ms-excel")
+            st.download_button(label="Download the results file", data=open(output_file, 'rb').read(), file_name=output_file, mime="application/vnd.ms-excel")
