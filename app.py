539d0e0903e77b04259d33c94b025984f9fccbcb
shaniangel <shaniangel97@gmail.com>
2024-10-12 16:08:09 +0300

    wider page layout + page title

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 # Set up the layout for navigation
+st.set_page_config(page_title="Terms and Services Query Interface", layout="wide")
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Query Interface", "Upload Excel"])
 
