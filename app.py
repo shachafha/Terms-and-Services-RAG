2b4a9ebbbb22f72dcbc303b84c446871a741de73
shaniangel <shaniangel97@gmail.com>
2024-10-31 15:00:35 +0200

    create pages layout

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 import streamlit as st
-
-# Set up the layout for navigation
-st.set_page_config(page_title="Terms and Services Query Interface", layout="wide")
+st.set_page_config(page_title="Terms and Services Query Interface", layout="wide", page_icon="logo.png")
 col1, col2 = st.columns([1, 10])
 col1.image('logo.jpeg')
 col2.title("SimplifAI T&S")
 col2.subheader("Terms and Services Query Interface")
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Chat", "Testing Environment"])
+chat = st.Page("app_chat.py", title="Chat", icon=":material/smart_toy:")
+test = st.Page("app_excel.py", title="Testing Environment", icon=":material/query_stats:")
 
-
-if page == "Chat":
-    from app_chat import main
-    main()
-else:
-    from app_excel import main
-    main()
-
+pg = st.navigation([chat, test])
+pg.run()
