5e62e4e658022922a4907f9a150604f4ae9fbc87
shachafhaviv <shachaf.haviv@gmail.com>
2024-11-08 11:23:01 +0200

    change 'chat' to 'query'

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ col1, col2 = st.columns([1, 10])
 col1.image('logo.jpeg')
 col2.title("SimplifAI T&S")
 col2.subheader("Terms and Services Query Interface")
-chat = st.Page("app_chat.py", title="Chat", icon=":material/smart_toy:")
+chat = st.Page("app_chat.py", title="Query", icon=":material/smart_toy:")
 test = st.Page("app_excel.py", title="Testing Environment", icon=":material/query_stats:")
 
 pg = st.navigation([chat, test])
